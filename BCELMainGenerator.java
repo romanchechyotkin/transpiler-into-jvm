942255532fcc2e0c6079d01a4cdf2b88d616edde
romanchechyotkin <romanchechyotkin@gmail.com>
2024-04-07 21:41:55 +0300

    java generation

---
+++ BCELMainGenerator.java
@@ -0,0 +1,37 @@
+import org.apache.bcel.Const;
+import org.apache.bcel.generic.*;
+import org.apache.bcel.classfile.*;
+
+public class BCELMainGenerator {
+
+    public static void main(String[] args) {
+        try {
+            generateMainClass("Main");
+            System.out.println("Main class generated successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void generateMainClass(String className) throws Exception {
+        ClassGen cg = new ClassGen(className, "java.lang.Object", "<generated>", Const.ACC_PUBLIC | Const.ACC_SUPER, null);
+        ConstantPoolGen cp = cg.getConstantPool();
+
+        // Create main method
+        InstructionList il = new InstructionList();
+        MethodGen mg = new MethodGen(Const.ACC_PUBLIC | Const.ACC_STATIC, Type.VOID, new Type[] { new ArrayType(Type.STRING, 1) }, new String[] { "args" }, "main", className, il, cp);
+
+        il.append(new GETSTATIC(cp.addFieldref("java.lang.System", "out", "Ljava/io/PrintStream;")));
+        il.append(new LDC(cp.addString("Hello, World!")));
+        il.append(new INVOKEVIRTUAL(cp.addMethodref("java.io.PrintStream", "println", "(Ljava/lang/String;)V")));
+        il.append(new RETURN());
+
+        mg.setMaxStack();
+        mg.setMaxLocals();
+        cg.addMethod(mg.getMethod());
+
+        // Write class to file
+        JavaClass javaClass = cg.getJavaClass();
+        javaClass.dump(className + ".class");
+    }
+}
--- src/repl/repl.rs
+++ src/repl/repl.rs
@@ -1,5 +1,6 @@
 use compiler_in_rust::parser::parser::Parser;
-use compiler_in_rust::generator::generator::Generator;
+use compiler_in_rust::generator::generator::{Generator, JVMInstruction};
+use std::process::{Command, Output};
 
 fn main() {
     let input = "
@@ -13,10 +14,53 @@ fn main() {
     let program = parser.parse_program();
 
     let mut generator: Generator = Generator::new();
-    
-    for stmt in &program {
-        let instructions = generator.generate_code(stmt);
-        dbg!(instructions);
-    }
 
+    let mut instructions: Vec<JVMInstruction> = Vec::new(); 
+
+    for stmt in &program {
+        let mut ins = generator.generate_code(stmt);
+        instructions.append(&mut ins);
+    }
+    
+    dbg!(&instructions);
+
+    cmd()
+}
+
+fn cmd() {
+    let mut output = Command::new("javac")
+        .arg("-cp")
+        .arg("bcel-6.8.2.jar")
+        .arg("BCELMainGenerator.java")
+        .output()
+        .expect("failed to run javac");
+
+    print_output(&output);
+
+
+    output = Command::new("java")
+        .arg("-cp")
+        .arg(".:bcel-6.8.2.jar")
+        .arg("BCELMainGenerator")
+        .output()
+        .expect("failed to run java");
+
+    print_output(&output);
+
+    output = Command::new("java")
+        .arg("Main")
+        .output()
+        .expect("failed to run java");
+
+    print_output(&output);
+}
+
+fn print_output(output: &Output) {
+    if output.status.success() {
+        println!("Command executed successfully!");
+        println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
+    } else {
+        println!("Command failed with status: {}", output.status);
+        println!("Error:\n{}", String::from_utf8_lossy(&output.stderr));
+    }
 }
